b0c7c7a4fe5b141e1936e1cc4bc7d5b5cf496da1
OYKLd <lydieouattara828@gmail.com>
2026-01-13 01:34:35 +0000

    V_04

--- stopwatch_app/lib/main.dart
+++ stopwatch_app/lib/main.dart
@@ -110,7 +110,7 @@ class _StopwatchScreenState extends State<StopwatchScreen> with SingleTickerProv
             Padding(
               padding: const EdgeInsets.only(top: 30.0),
               child: Text(
-                'Chrono',
+                'Chronomètre',
                 style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                   color: const Color(0xFF212529),
                   fontWeight: FontWeight.bold,
